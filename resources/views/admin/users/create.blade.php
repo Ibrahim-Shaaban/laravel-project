eebce4090adddb656791b35a3b35537ec1647279
Hema <hemaabelnaby325@gmail.com>
2018-05-22 22:49:47 +0200

    send data from and to users table

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,7 +1,52 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Hema
- * Date: 5/22/2018
- * Time: 4:38 PM
- */
+@extends('layouts.admin')
+
+@section('content')
+    <h1>
+        create user
+    </h1>
+
+    {!!Form::open(['method'=>'POST' , 'action'=>'AdminUsersController@store' , 'files'=>true])!!}
+
+
+
+            <div class="form-group">
+                {!! Form::label('name' , 'Name :') !!}
+                {!! Form::text('name' , null , ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('email' , 'Email :') !!}
+                {!! Form::email('email' , null , ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('role_id' , 'role_id :') !!}
+                {!! Form::select('role_id' ,$roles ,null , ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('is_active' , 'status :') !!}
+                {!! Form::select('is_active' ,array(1=>'Active' , 0=>'Not Active'), 0 , ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('file' , 'file :') !!}
+                {!! Form::file('file' ,null ,['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('password' , 'password :') !!}
+                {!! Form::password('password' , ['class'=>'form-control']) !!}
+            </div>
+            {!! Form::submit('Add User' ,['class'=>'btn btn-primary']) !!}
+
+
+
+
+
+        {!! Form::close() !!}
+
+        @include('includes.form_error')
+
+
+@endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,3 +1,38 @@
-<h1>
-    afrawy
-</h1>
+@extends('layouts.admin')
+
+@section('content')
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+              <th>Role</th>
+              <th>Status</th>
+              <th>created at</th>
+              <th>updated at</th>
+          </tr>
+        </thead>
+        <tbody>
+
+            @if(count($users))
+
+                @foreach($users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->role->name}}</td>
+                        <td>{{$user->is_active == 1 ? 'active' : 'not active'}}</td>
+                        <td>{{$user->created_at}}</td>
+                        <td>{{$user->updated_at}}</td>
+                    </tr>
+                @endforeach
+
+            @endif
+
+        </tbody>
+      </table>
+
+@endsection
